8f9899b4bdca5c31b3a77678860407ed03a4beec
JaymarM <jaymar28@hotmail.com>
2026-01-18 03:57:09 -0500

    Apunte a produccion

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api'
+// };
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://portalbejarano.onrender.com/api'
 };
